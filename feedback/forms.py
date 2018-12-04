c283719f35b3933cd9dc741db52f91157677bfd2
Timothy Hobbs <timothy@hobbs.cz>
2018-12-04 19:54:53 +0100

    Add simple text captcha support

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 from django import forms
+from django.conf import settings
 from django.contrib.sites.models import Site
+from django.utils.translation import ugettext_lazy as _
 
 from feedback.models import Feedback
 
 class FeedbackForm(forms.ModelForm):
     '''The form shown when giving feedback'''
+    captchas = getattr(settings, 'FEEDBACK_CAPTCHAS', {})
+    if captchas:
+        captchaquestion = forms.CharField()
+        captcha = forms.CharField()
+
+    def clean_captcha(self):
+       for question, answers in self.captchas:
+           if self.cleaned_data['captchaquestion'] == question and self.cleaned_data['captcha'] in answers:
+               return self.cleaned_data['captcha']
+       if self.captchas:
+           raise forms.ValidationError(_("Captcha incorrect"))
+       return self.cleaned_data['captcha']
+
     class Meta:
         model = Feedback
         fields = "__all__"
